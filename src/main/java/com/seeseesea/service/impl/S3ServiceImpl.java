2dc920f58aa0e776c329f902d39362389d1f6f4a
liuchenglong <liuchenglong125@foxmail.com>
2025-08-19 16:24:18 +0800

    2025-08-19 16:24:18

--- src/main/java/com/seeseesea/service/impl/S3ServiceImpl.java
+++ src/main/java/com/seeseesea/service/impl/S3ServiceImpl.java
@@ -28,6 +28,7 @@ public class S3ServiceImpl implements S3Service {
     @Override
     public String uploadFile(MultipartFile file, String key) {
         try {
+            log.info("开始上传文件: {}, {}, {}", key, ossProperties.getBucket(), file.getContentType());
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(ossProperties.getBucket())
                     .key(key)
